09031b40ec3a17ae8d8bd82b4ce012fb58a6ab0f
Panela Leung <leungpanela@gmail.com>
2019-05-15 14:22:21 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -2,10 +2,5 @@
 def say_hello(name="Ruby Programmer")
   puts "Hello,  #{name} !"
 end
-<<<<<<< HEAD
-say_hello("sam")
+say_hello("Gabriela")
 say_hello()
-=======
-say_hello ("sam")
-say_hello ()
->>>>>>> 6cbb58c5d94cfbbe807074f65c99c0a06b11475e
